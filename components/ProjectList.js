960f0767bc4fbb70bda9359412a091377c77dc13
JohanOgren <ogren.johan@edu.edugrade.se>
2022-03-09 15:17:31 +0100

    Inför Tobias

--- components/ProjectList.js
+++ components/ProjectList.js
@@ -1,11 +1,19 @@
+import styles from '../styles/Home.module.css'
+import Button from '../components/Button'
+
 const ProjectList = ({ projects }) => {
   return (
-    <div>
-      <h1>Aktiva Projekt</h1>
+    <div className={styles.container}>
+      <h1>Aktiva Projekt ☺</h1>
       <ul>
         {projects.map((project) => (
-          <li key={project.id}>
+          <li key={project.id} className={styles.card}>
             {project.properties.Projectname.title[0].plain_text}
+            {/* <Button
+              color={'green'}
+              text={'Add'}
+              onClick={}
+            /> */}
           </li>
         ))}
       </ul>
