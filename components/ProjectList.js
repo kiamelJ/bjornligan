44eb4f69fca8870b0c3dedba13c6c148ec2e6a33
AxelssonOscar <100521490+AxelssonOscar@users.noreply.github.com>
2022-03-21 10:29:57 +0100

    start av refaktorering

--- components/ProjectList.js
+++ components/ProjectList.js
@@ -1,14 +1,62 @@
-const ProjectList = ({ projects }) => {
+import react from "react";
+import { useEffect, useState } from "react";
+import { setCookies, getCookie } from "cookies-next";
+import router from "next/router";
+
+const ProjectList = () => {
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+  
+  useEffect(() => {
+    setLoading(true);
+    fetch("../api/people", {
+      method: "POST",
+      headers: {
+        "Content-Type": "plain/text",
+      },
+      body: getCookie("Björnligan"),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+        //console.log(data);
+      });
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (!data) return <p>No profile data</p>;
+
+  function makeTimereport(projectId) {
+
+    console.log(projectId)
+    setCookies("Project.Id", projectId)
+    router.push("../timereport");
+  }
+
   return (
-    <div>
-      <h1>Aktiva Projekt</h1>
-      <ul>
-        {projects.map((project) => (
-          <li key={project.id}>
-            {project.properties.Projectname.title[0].plain_text}
-          </li>
-        ))}
-      </ul>
+    <div className='container'>
+      <main className='main'>
+        <h1 className='title'>Aktiva Projekt</h1>
+        <p className='description'></p>
+        <div className='grid'>
+          {data.map((project) => (
+            <li key={project.id} id={project.id} className='card'>
+              <h2>
+                {project.properties.Projectname.title[0].plain_text}
+              </h2>
+              {/* <p>projektinfo</p> */}
+              {/* <button onClick={makeTimereport(project.id)}>Ny tidrapport</button> */}
+              <button type="submit" onClick={() => { makeTimereport(project.id) }}>Ny tidrapport</button>
+              {/* <Link href='../timereport'>
+                <a>
+                  <button onClick={makeTimereport(project.id)}>Ny tidrapport</button>
+                </a>
+              </Link> */}
+            </li>
+          ))}
+        </div>
+      </main>
     </div>
   );
 };
--- pages/project/index.js
+++ pages/project/index.js
@@ -1,74 +1,9 @@
-import react from "react";
-import { useEffect, useState } from "react";
-import Link from "next/link";
-import FormTimeReport from "../timereport";
-import { setCookies, getCookie } from "cookies-next";
-import router from "next/router";
+import ProjectListComponent from '../../components/ProjectList'
 
-const ProjectList = ({ projects }) => {
-  const [data, setData] = useState(null);
-  const [isLoading, setLoading] = useState(false);
-
-
-
-  useEffect(() => {
-    setLoading(true);
-    fetch("../api/people", {
-      method: "POST",
-      headers: {
-        "Content-Type": "plain/text",
-      },
-      body: getCookie("Björnligan"),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        setLoading(false);
-        //console.log(data);
-      });
-  }, []);
-
-  if (isLoading) return <p>Loading...</p>;
-  if (!data) return <p>No profile data</p>;
-
-  function makeTimereport(projectId) {
-
-    console.log(projectId)
-    setCookies("Project.Id", projectId)
-    router.push("../timereport");
-  }
-
-
-  return (
-    <div className='container'>
-      <main className='main'>
-        <h1 className='title'>Aktiva Projekt</h1>
-        <p className='description'></p>
-        <div className='grid'>
-          {data.map((project) => (
-            <li key={project.id} id={project.id} className='card'>
-              <h2>
-                {project.properties.Projectname.title[0].plain_text}
-              </h2>
-              {/* <p>projektinfo</p> */}
-              {/* <button onClick={makeTimereport(project.id)}>Ny tidrapport</button> */}
-              <button type="submit" onClick={() => { makeTimereport(project.id) }}>Ny tidrapport</button>
-              {/* <Link href='../timereport'>
-                <a>
-                  <button onClick={makeTimereport(project.id)}>Ny tidrapport</button>
-                </a>
-              </Link> */}
-            </li>
-          ))}
-        </div>
-      </main>
-    </div>
-  );
+const ProjectList = () => {
+  return(
+    <ProjectListComponent />
+  )
 };
-// const makeTimereport=function(projectId){
-//   console.log(projectId)
-//   setCookies("Project.Id",projectId)
-
-// }
 
 export default ProjectList;
