e90101c9c2f835c4f04cbce1a13c2ed48b9a48fe
1Ante <100119985+1Ante@users.noreply.github.com>
2022-03-23 15:08:14 +0100

    Logout css

--- components/Nav.js
+++ components/Nav.js
@@ -4,19 +4,15 @@ import Link from 'next/link'
 const Nav = () => {
   return (
     <nav className={navStyles.nav}>
-        <ul>
-            <li>
-                {/*TODO: Fixa */}
-                <Link href='/'>Logga ut</Link>
-            </li>
+        <ul>           
             <li>
                 <Link href='/project'>Projekt</Link>
-            </li>
-            {/* <li>
-                <Link href='/timereport'>Rapporter</Link>
-            </li> */}
-            <li>
+            </li>           
+            <li className={navStyles.rightMargin}>
                 <Link href='/userpage'>Min sida</Link>
+            </li>
+            <li>                
+                <Link href='/'>Logga ut</Link>
             </li>
         </ul>
     </nav>
