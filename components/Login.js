b840e7ac3249d585328d32144eb4b927e733453c
JohanOgren <ogren.johan@edu.edugrade.se>
2022-03-24 15:11:08 +0100

    update English Alerts

--- components/Login.js
+++ components/Login.js
@@ -34,7 +34,7 @@ function Login() {
       })
       .catch((error) => {
         console.log("Something went wrong.", error);
-        alert("Fel login");
+        alert("Invalid credentials. Please try again.");
       });
   }
 
@@ -43,34 +43,34 @@ function Login() {
       <form className={styles.form} onSubmit={LoginUser}>
         <div className={styles.inputs}>
           <div>
-          <label htmlFor='username'>Username</label>
-          <input
-            name='username'
-            type='text'
-            placeholder='Enter username...'
-            //TODO: Fixa regex
-            // pattern='([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))'
-            // title='(YYYY-MM-DD)'
-            value={data.username}
-            onChange={(e) =>
-              setData({ ...data, [e.target.name]: e.target.value })
-            }
-            required
-          />
+            <label htmlFor="username">Username</label>
+            <input
+              name="username"
+              type="text"
+              placeholder="Enter username..."
+              //TODO: Fixa regex
+              // pattern='([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))'
+              // title='(YYYY-MM-DD)'
+              value={data.username}
+              onChange={(e) =>
+                setData({ ...data, [e.target.name]: e.target.value })
+              }
+              required
+            />
           </div>
         </div>
-        <label htmlFor='password'>Password</label>
+        <label htmlFor="password">Password</label>
         <input
-          name='password'
-          type='password'
-          placeholder='Enter password...'
+          name="password"
+          type="password"
+          placeholder="Enter password..."
           value={data.password}
           onChange={(e) =>
             setData({ ...data, [e.target.name]: e.target.value })
           }
           required
         />
-        <button type='submit'>Login</button>
+        <button type="submit">Login</button>
       </form>
     </div>
   );
--- components/ReportCreate.js
+++ components/ReportCreate.js
@@ -26,7 +26,7 @@ const ReportCreate = () => {
       body: JSON.stringify(data),
     }).then((response) => {
       if (response.ok) {
-        alert("Tidsrapporten är skapad.");
+        alert("Your report has been submitted");
       }
     });
   }
@@ -36,41 +36,41 @@ const ReportCreate = () => {
       <form className={styles.form} onSubmit={submitReport}>
         <div className={styles.inputs}>
           <div>
-          <label htmlFor='note'>Note</label>
-          <input
-            name='note'
-            type='text'
-            placeholder='Enter comment...'
-            value={data.note}
-            onChange={(e) =>
-              setData({ ...data, [e.target.name]: e.target.value })
-            }
-            required
-          />
+            <label htmlFor="note">Note</label>
+            <input
+              name="note"
+              type="text"
+              placeholder="Enter comment..."
+              value={data.note}
+              onChange={(e) =>
+                setData({ ...data, [e.target.name]: e.target.value })
+              }
+              required
+            />
           </div>
         </div>
 
-        <label htmlFor='date'>Date</label>
+        <label htmlFor="date">Date</label>
         <DatePicker
-          dateFormat='yyyy-MM-dd'
+          dateFormat="yyyy-MM-dd"
           selected={data.date}
           onChange={(e) => setData({ ...data, date: e })}
           required
         />
 
-        <label htmlFor='hour'>Hours</label>
+        <label htmlFor="hour">Hours</label>
         <input
-          name='hour'
-          type='number'
-          placeholder='Enter hours...'
-          pattern='^[0-9]*$'
+          name="hour"
+          type="number"
+          placeholder="Enter hours..."
+          pattern="^[0-9]*$"
           value={data.hours}
           onChange={(e) =>
             setData({ ...data, [e.target.name]: e.target.value })
           }
           required
         />
-        <button type='submit'>Submit</button>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
