54bc4c94167b469449452a7b68b8793fbb436770
JohanOgren <ogren.johan@edu.edugrade.se>
2022-03-29 16:12:10 +0200

    Fix- Language on Profile

--- components/UserPage.js
+++ components/UserPage.js
@@ -1,19 +1,16 @@
 import React from "react";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { getCookie } from "cookies-next";
 
-
-import Loader from './Loader'
+import Loader from "./Loader";
 
 const UserPage = () => {
   const [data, setData] = useState(null);
   const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
-
-
-    setLoading(true)
-    fetch('../api/people/getuser', {
+    setLoading(true);
+    fetch("../api/people/getuser", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -22,34 +19,28 @@ const UserPage = () => {
     })
       .then((res) => res.json())
       .then((data) => {
-        setData(data)
-        setLoading(false)
-        console.log(data)
-      })
-  }, [])
+        setData(data);
+        setLoading(false);
+        console.log(data);
+      });
+  }, []);
 
-
-  
   if (isLoading) return <Loader />;
-  if(!data) return <h1>no data</h1>
-
+  if (!data) return <h1>no data</h1>;
 
   return (
-    
-    <div className="container">
-    <main className="main">
-      <h1 className="title">{data.properties.Name.title[0].plain_text}</h1>
-      Användarnamn: {data.properties.Username.rich_text[0].plain_text}<br/>
-      Antal projekt: {data.properties.Projects.relation.length}<br/>
-
-    </main>
-  </div>
-)
+    <div className='container'>
+      <main className='main'>
+        <h1 className='title'>{data.properties.Name.title[0].plain_text}</h1>
+        Username: {data.properties.Username.rich_text[0].plain_text}
+        <br />
+        Number of projects: {data.properties.Projects.relation.length}
+        <br />
+      </main>
+    </div>
+  );
 };
 
-
-function draw() {
-  
-}
+function draw() {}
 
 export default UserPage;
