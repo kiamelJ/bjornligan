c490d46e4a59e1fa72eaee18451d899e95dafa77
1Ante <andreas.naslund@edu.edugrade.se>
2022-03-29 13:00:33 +0200

    Metadata för "Project summary", "Report Summary"

--- pages/project/projectsummary.js
+++ pages/project/projectsummary.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Layout from "../../components/Layout";
 import ProjectSummary from "../../components/ProjectSummary";
+import Meta from "../../components/Meta";
 
 
 const report = ({ project }) => {
   return (
     <>
       <Layout>
-        <ProjectSummary project={project} />
+      <Meta title='Project Summary' keywords='users, time, date, project' description='Total worked hours for a specific project at a specific date/period' />
+      <ProjectSummary project={project} />
       </Layout>
     </>
   );
--- pages/timereport/reportsummary.js
+++ pages/timereport/reportsummary.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReportSummaryComponent from "../../components/ReportSummary";
 import Layout from "../../components/Layout";
+import Meta from "../../components/Meta";
 
 const ReportSummary = () => {
   return (
     <>
       <Layout>
-        <ReportSummaryComponent />
+      <Meta title='Report Summary' keywords='user, time, date' description='Worked hours for each employee at a specific date/period' />
+      <ReportSummaryComponent />
       </Layout>
     </>
   );
